db9c7043d3c5073e5b8c1b9d04feca392c32de03
Adnan khan <adnanhabibkhan98@gmail.com>
2023-01-04 15:37:35 +0500

    padding in ios

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1049,7 +1049,7 @@ class _HomeState extends State<Home> {
     return Dialog(
       alignment: Alignment.topRight,
       insetPadding: Platform.isIOS
-          ? EdgeInsets.only(right: 50, top: 110, left: 14)
+          ? EdgeInsets.only(right: 50, top: 90, left: 14)
           : EdgeInsets.only(right: 50, top: 115, left: 14),
       child: Container(
           height: 35,
